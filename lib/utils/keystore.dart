e48039e0678b806e8cd140be924e94bf7da56cf3
Zubair Ahmad <zubairahmadcontact@gmail.com>
2025-02-01 14:11:39 +0500

    Add new error notifying mechanism, provide doc comments and use error notifying mechanims

--- lib/utils/keystore.dart
+++ lib/utils/keystore.dart
@@ -1,10 +1,12 @@
+import 'package:github_var_updater/utils/app_notifier.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// This class helps to store key-value pairs that persists
-// even after app is closed. They are stored locally.
+/// This class helps to store key-value pairs that persists
+/// even after app is closed. They are stored locally and will
+/// be cleared if app's data is cleared.
 class Keystore {
 
-  // Stores the given key value pair
+  /// Stores the given key value pair. Doesn't throws exception
   static Future<void> savePair({required String key, required dynamic value}) async {
     try {
       SharedPreferences preference = await SharedPreferences.getInstance();
@@ -24,23 +26,33 @@ class Keystore {
         throw UnsupportedError('The type "${value.runtimeType}" isn\'t supported!');
       }
     } catch (e) {
-      print('Exception occured: ${e.toString()}');
+      AppNotifier.showErrorDialog(errorMessage: e.toString());
     }
   }
 
-  // Retrives the value associated with given key. Returns null if can't find.
+  /// Retrives the value associated with given key. Returns null if can't find.
+  /// 
+  /// Doesn't throw exception
   static Future<dynamic> getValueForKey({required String key}) async {
     try {
       SharedPreferences pref = await SharedPreferences.getInstance();
       return pref.get(key);
     } catch (e) {
-      print('Exception occured: ${e.toString()}');
+      AppNotifier.showErrorDialog(errorMessage: e.toString());
     }
     return null;
   }
 
-  // Tells whether given key is present in the keystore
+  /// Tells whether given key is present in the keystore
+  /// 
+  /// Doesn't throw exception
   static Future<bool> contains({required String key}) async {
-    return (await SharedPreferences.getInstance()).containsKey(key);
+    try {
+      SharedPreferences pref = await SharedPreferences.getInstance();
+      return pref.containsKey(key);
+    } catch (e) {
+      AppNotifier.showErrorDialog(errorMessage: e.toString());
+    }
+    return false;
   }
 }
